78279bc15c77c348e0e775fcb51ac8042ad3b95d
FiDai <hoangphidai123@gmail.com>
2021-03-27 17:40:19 +0700

    add data wrapper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Http\Resources\CategoryResource;
 use App\Http\Resources\ProductResource;
 use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 use Illuminate\Support\ServiceProvider;
@@ -25,5 +26,6 @@ class AppServiceProvider extends ServiceProvider {
      */
     public function boot() {
         ProductResource::withoutWrapping();
+        CategoryResource::wrap('data');
     }
 }
