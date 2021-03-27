ac3632079d350abe7b4023453e43dd38b47c2c22
FiDai <hoangphidai123@gmail.com>
2021-03-27 19:27:15 +0700

    update rating api

--- app/Http/Resources/RatingResource.php
+++ app/Http/Resources/RatingResource.php
@@ -5,16 +5,14 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class RatingResource extends JsonResource
-{
+class RatingResource extends JsonResource {
     /**
      * Transform the resource into an array.
      *
-     * @param  Request  $request
+     * @param Request $request
      * @return array
      */
-    public function toArray($request)
-    {
+    public function toArray($request) {
         return [
             'id' => $this->id,
             'rating' => $this->rating,
--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -6,6 +6,7 @@ use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Carbon;
 
 /**
@@ -41,11 +42,11 @@ class Rating extends Model {
 
     public $timestamps = true;
 
-    public function product() {
+    public function product(): BelongsTo {
         return $this->belongsTo(Product::class);
     }
 
-    public function user() {
+    public function user(): BelongsTo {
         return $this->belongsTo(User::class);
     }
 }
